a2468961742af1906b97403858e0f41789a29591
Halina Tracey <halina.tracey@gmail.com>
2019-11-22 12:31:31 -0800

    updating test file

---
+++ app.py
@@ -0,0 +1,5 @@
+def return3():
+    return 3
+
+if __name__=='__main__':
+    return3()
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import unittest
+import app
 
 class TestAction(unittest.TestCase):
     def test_assert(self):
-        assert 3 == 3
+        assert app.return3() == 3
 if __name__=='__main__':
     unittest.main()
